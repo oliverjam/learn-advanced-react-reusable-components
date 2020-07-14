118e0becc96b0bb8c054dd699b4c14b99e87b53c
oliverjam <oliverjamesphillips@gmail.com>
2020-07-14 12:31:20 +0100

    Reduce label duplication

--- src/solutions/2.js
+++ src/solutions/2.js
@@ -1,27 +1,21 @@
 import React from "react";
 
 function InputField({ id, label, info, showLabelBelow, ...rest }) {
+  const labels = (
+    <>
+      <label className="inputField__label" htmlFor={id}>
+        {label}
+      </label>
+      <div className="inputField__info" id={id + "-info"}>
+        {info}
+      </div>
+    </>
+  );
   return (
     <div className="inputField">
-      {!showLabelBelow && (
-        <label className="inputField__label" htmlFor={id}>
-          {label}
-          <div className="inputField__info" id={id + "-info"}>
-            {info}
-          </div>
-        </label>
-      )}
-
+      {!showLabelBelow && labels}
       <input className="inputField__input" id={id} {...rest} />
-
-      {showLabelBelow && (
-        <label className="inputField__label" htmlFor={id}>
-          <div>{label}</div>
-          <div className="inputField__info" id={id + "-info"}>
-            {info}
-          </div>
-        </label>
-      )}
+      {showLabelBelow && labels}
     </div>
   );
 }
